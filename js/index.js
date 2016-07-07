4cf583f9fab6201f54bddfb917654ee833c8d415
xingjiajia <1027288391@qq.com>
2016-07-07 23:29:32 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function(){
+  var mySwiper = new Swiper('.swiper-container', {
+	autoplay: 1000,//可选选项，自动滑动
+  speed:500,
+  loop:true,
+  pagination: '.swiper-pagination',
+  })
+  var mySwiper = new Swiper('#swiper-text', {
+	autoplay: 1000,//可选选项，自动滑动
+  speed:500,
+  loop:true,
+  pagination: '.swiper-pagination',
+  })
+})
